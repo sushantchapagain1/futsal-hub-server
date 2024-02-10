c0a3064b25cb1f8151fe3d1c9acca7684f9e11c5
trylow10 <aryaltrylowchan@gmail.com>
2024-02-10 21:18:05 +0545

    Remove Prisma-related files and update dependencies

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+// import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class AppService {
+  // constructor(private prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
